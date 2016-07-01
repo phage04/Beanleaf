353cb1b7fd33ccdc0dfda0cde630241527970d78
lylejover <lylejover2yahoo.com>
2016-07-01 17:13:26 +0200

    First

---
+++ Wiesenlust/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wiesenlust
+//
+//  Created by Lyle Christianne Jover on 01/07/2016.
+//  Copyright © 2016 Wiesenlust. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
