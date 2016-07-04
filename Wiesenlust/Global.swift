663044db143b69e45cfe14de95aeba95f95c4ad2
lylejover <lylejover2yahoo.com>
2016-07-04 13:12:27 +0200

    Launchscreen and initial setup

---
+++ Wiesenlust/Global.swift
@@ -0,0 +1,17 @@
+//
+//  Global.swift
+//  Wiesenlust
+//
+//  Created by Lyle Christianne Jover on 04/07/2016.
+//  Copyright © 2016 Wiesenlust. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreLocation
+import SystemConfiguration
+
+let COLOR1 = UIColor(red: CGFloat(188.0 / 255.0), green: CGFloat(208.0 / 255.0), blue: CGFloat(24.0 / 255.0), alpha: 1.0)
+let COLOR2 = UIColor(red: CGFloat(146.0 / 255.0), green: CGFloat(20.0 / 255.0), blue: CGFloat(114.0 / 255.0), alpha: 1.0)
+
+let logo1 = "logo02.png"
---
+++ Wiesenlust/Home.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wiesenlust
+//
+//  Created by Lyle Christianne Jover on 01/07/2016.
+//  Copyright © 2016 Wiesenlust. All rights reserved.
+//
+
+import UIKit
+
+class Home: UIViewController {
+
+    @IBOutlet var backgroundView: UIView!    
+    @IBOutlet weak var logo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        backgroundView.backgroundColor = COLOR1
+    }
+
+    
+
+
+}
+
