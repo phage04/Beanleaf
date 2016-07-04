41462651285072a6ed66ef1fd4422e708c9bf27a
lylejover <lylejover2yahoo.com>
2016-07-04 15:43:22 +0200

    Home menu completed

--- Wiesenlust/Global.swift
+++ Wiesenlust/Global.swift
@@ -13,5 +13,25 @@ import SystemConfiguration
 
 let COLOR1 = UIColor(red: CGFloat(188.0 / 255.0), green: CGFloat(208.0 / 255.0), blue: CGFloat(24.0 / 255.0), alpha: 1.0)
 let COLOR2 = UIColor(red: CGFloat(146.0 / 255.0), green: CGFloat(20.0 / 255.0), blue: CGFloat(114.0 / 255.0), alpha: 1.0)
+let COLOR2_DARK = UIColor(red: CGFloat(105.0 / 255.0), green: CGFloat(14.0 / 255.0), blue: CGFloat(82.0 / 255.0), alpha: 1.0)
 
-let logo1 = "logo02.png"
+let logo1 = UIImage(named: "logo02.png")
+let menuIcon1 = UIImage(named: "menuIcon1.png")
+let menuIcon2 = UIImage(named: "menuIcon2.png")
+let menuIcon3 = UIImage(named: "menuIcon3.png")
+let menuIcon4 = UIImage(named: "menuIcon4.png")
+let menuIcon5 = UIImage(named: "menuIcon5.png")
+let menuIcon6 = UIImage(named: "menuIcon6.png")
+
+let font1Thin = "HelveticaNeue-Thin"
+let font1Medium = "HelveticaNeue-Medium"
+let font1Regular = "HelveticaNeue-Regular"
+let font1Light = "HelveticaNeue-Light"
+let font1UltraLight = "HelveticaNeue-UltraLight"
+
+let menuLblText1 = "Menu"
+let menuLblText2 = "Coupons"
+let menuLblText3 = "Rewards"
+let menuLblText4 = "Reservations"
+let menuLblText5 = "Locations"
+let menuLblText6 = "Feedback"
--- Wiesenlust/Home.swift
+++ Wiesenlust/Home.swift
@@ -13,13 +13,138 @@ class Home: UIViewController {
     @IBOutlet var backgroundView: UIView!    
     @IBOutlet weak var logo: UIImageView!
     
+    @IBOutlet weak var menuItem1: UIButton!
+    
+    @IBOutlet weak var menuItem2: UIButton!
+    
+    @IBOutlet weak var menuItem3: UIButton!
+    
+    @IBOutlet weak var menuItem4: UIButton!
+    
+    @IBOutlet weak var menuItem5: UIButton!
+    
+    @IBOutlet weak var menuItem6: UIButton!
+
+    @IBOutlet weak var menuLbl1: UILabel!
+    
+    @IBOutlet weak var menuLbl2: UILabel!
+    
+    @IBOutlet weak var menuLbl3: UILabel!
+    
+    @IBOutlet weak var menuLbl4: UILabel!
+    
+    @IBOutlet weak var menuLbl5: UILabel!
+    
+    @IBOutlet weak var menuLbl6: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundView.backgroundColor = COLOR1
+        
+        var image = menuIcon1!
+        let targetWidth : CGFloat = 92
+        let targetHeight : CGFloat = 92
+        var scaledImage = image
+        
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(targetWidth, targetHeight),false,0)
+        image.drawInRect(CGRectMake(0, 0, targetWidth, targetHeight))
+        scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        menuItem1.setImage(scaledImage.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
+   
+        image = menuIcon2!
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(targetWidth, targetHeight),false,0)
+        image.drawInRect(CGRectMake(0, 0, targetWidth, targetHeight))
+        scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        menuItem2.setImage(scaledImage.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
+        
+        image = menuIcon3!
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(targetWidth, targetHeight),false,0)
+        image.drawInRect(CGRectMake(0, 0, targetWidth, targetHeight))
+        scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        menuItem3.setImage(scaledImage.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
+        
+        image = menuIcon4!
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(targetWidth, targetHeight),false,0)
+        image.drawInRect(CGRectMake(0, 0, targetWidth, targetHeight))
+        scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        menuItem4.setImage(scaledImage.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
+        
+        image = menuIcon5!
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(targetWidth, targetHeight),false,0)
+        image.drawInRect(CGRectMake(0, 0, targetWidth, targetHeight))
+        scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        menuItem5.setImage(scaledImage.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
+        
+        image = menuIcon6!
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(targetWidth, targetHeight),false,0)
+        image.drawInRect(CGRectMake(0, 0, targetWidth, targetHeight))
+        scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        menuItem6.setImage(scaledImage.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
+        
+        
+        menuItem1.tintColor = COLOR2
+        menuItem2.tintColor = COLOR2
+        menuItem3.tintColor = COLOR2
+        menuItem4.tintColor = COLOR2
+        menuItem5.tintColor = COLOR2
+        menuItem6.tintColor = COLOR2
+        
+        menuLbl1.font = UIFont(name: font1Medium, size: 14)
+        menuLbl2.font = UIFont(name: font1Medium, size: 14)
+        menuLbl3.font = UIFont(name: font1Medium, size: 14)
+        menuLbl4.font = UIFont(name: font1Medium, size: 14)
+        menuLbl5.font = UIFont(name: font1Medium, size: 14)
+        menuLbl6.font = UIFont(name: font1Medium, size: 14)
+        
+        menuLbl1.textColor = COLOR2
+        menuLbl2.textColor = COLOR2
+        menuLbl3.textColor = COLOR2
+        menuLbl4.textColor = COLOR2
+        menuLbl5.textColor = COLOR2
+        menuLbl6.textColor = COLOR2
+        
+        menuLbl1.text = menuLblText1
+        menuLbl2.text = menuLblText2
+        menuLbl3.text = menuLblText3
+        menuLbl4.text = menuLblText4
+        menuLbl5.text = menuLblText5
+        menuLbl6.text = menuLblText6
+        
+        
+        
+
+        
     }
 
+   
+    @IBAction func menuItem1Pressed(sender: AnyObject) {
+        
+    }
     
+    @IBAction func menuItem2Pressed(sender: AnyObject) {
+    }
 
+    @IBAction func menuItem3Pressed(sender: AnyObject) {
+    }
 
+    @IBAction func menuItem4Pressed(sender: AnyObject) {
+    }
+    
+    @IBAction func menuItem5Pressed(sender: AnyObject) {
+    }
+
+    @IBAction func menuItem6Pressed(sender: AnyObject) {
+    }
+    
+    
+    
+    
 }
 
