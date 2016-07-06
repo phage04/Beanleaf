a709d43747042e507a733c9e561188c69d20cdf6
lylejover <lylejover2yahoo.com>
2016-07-06 16:36:08 +0200

    added animation to item view

--- Wiesenlust/ItemView.swift
+++ Wiesenlust/ItemView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ItemView: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ItemView: UIViewController, UITableViewDelegate, UITableViewDataSource,  UIScrollViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -28,13 +28,24 @@ class ItemView: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var dishName = ""
     var dishDesc = "Lorem ipsum keme keme keme 48 years balaj sudems at sangkatuts at jowabelles jowabelles ng at bakit na ang kasi shogal chapter at ang keme borta kasi at bakit doonek fayatollah kumenis at nang wiz at bakit valaj chuckie at bakit tetetet na valaj wiz at bakit matod nang shonga shongaers bakit na ang chaka valaj shonga at ang waz kemerloo shonget nang na buya bongga kasi ang balaj tetetet biway sa planggana antibiotic pinkalou klapeypey-klapeypey shonget nang buya tungril cheapangga bonggakea mahogany at Mike ang chopopo bakit na ang nang at nang , at bakit dites katol jowabelles nang chipipay dites shogal sa at pamenthol ma-kyonget emena gushung ganders at ang nang na ang shala balaj guash shontis ng oblation na ang majubis at bakit. Lorem ipsum keme keme keme 48 years balaj sudems at sangkatuts at jowabelles jowabelles ng at bakit na ang kasi shogal chapter at ang keme borta kasi at bakit doonek fayatollah kumenis at nang wiz at bakit valaj chuckie at bakit tetetet na valaj wiz at bakit matod nang shonga shongaers bakit na ang chaka valaj shonga at ang waz kemerloo shonget nang na buya bongga kasi ang balaj tetetet biway sa planggana antibiotic pinkalou klapeypey-klapeypey shonget nang buya tungril cheapangga bonggakea mahogany at Mike ang chopopo bakit na ang nang at nang , at bakit dites katol jowabelles nang chipipay dites shogal sa at pamenthol ma-kyonget emena gushung ganders at ang nang na ang shala balaj guash shontis ng oblation na ang majubis at bakit."
     
+    private let kTableHeaderHeight: CGFloat = 300.0
+    var headerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
+
         self.tableView.backgroundColor = UIColor.whiteColor()
         
+        headerView = self.tableView.tableHeaderView
+        self.tableView.tableHeaderView = nil
+        self.tableView.addSubview(headerView)
+        
+        self.tableView.contentInset = UIEdgeInsets(top: kTableHeaderHeight, left: 0, bottom: 0, right: 0)
+        self.tableView.contentOffset = CGPoint(x: 0, y: -kTableHeaderHeight)
+
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         
         navigationItem.title = dishName
 
@@ -55,7 +66,33 @@ class ItemView: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         circleView.layer.cornerRadius = circleView.frame.size.width / 2
         circleView.backgroundColor = COLOR2
+        updateHeaderView()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        updateHeaderView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateHeaderView()
+    }    
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        updateHeaderView()
+    }
+    
+    func updateHeaderView() {
+        var headerRect = CGRect(x: 0, y: -kTableHeaderHeight, width: self.tableView.bounds.width, height: kTableHeaderHeight)
+        print("CO: \(self.tableView.contentOffset.y)")
+        print("kHeight: \(-kTableHeaderHeight)")
+        if self.tableView.contentOffset.y < -kTableHeaderHeight {
+            headerRect.origin.y = self.tableView.contentOffset.y
+            headerRect.size.height = -self.tableView.contentOffset.y
+        }
         
+        headerView.frame = headerRect
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
