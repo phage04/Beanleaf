63ce05e24da6de918fe53f82375231298124a3a4
lylejover <lylejover2yahoo.com>
2016-07-13 18:14:16 +0200

    Boiler plate for reservations.

---
+++ Wiesenlust/Reservations.swift
@@ -0,0 +1,28 @@
+//
+//  Reservations.swift
+//  Wiesenlust
+//
+//  Created by Lyle Christianne Jover on 13/07/2016.
+//  Copyright © 2016 Wiesenlust. All rights reserved.
+//
+
+import UIKit
+
+class Reservations: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem =
+            UIBarButtonItem(image:UIImage(named: "backBtn1x.png"), style:.Plain, target:self, action:#selector(Reservations.backButtonPressed(_:)));
+        
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(image:UIImage(named: "menuBtn1x.png"), style:.Plain, target:self, action:nil)
+        
+    }
+    
+    
+    func backButtonPressed(sender:UIButton) {
+        navigationController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
