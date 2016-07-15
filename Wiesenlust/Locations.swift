709362c88bc87c5b2f33ec808fea0c84f7e15564
lylejover <lylejover2yahoo.com>
2016-07-15 12:46:02 +0200

    Locations boilerplate setup

---
+++ Wiesenlust/Locations.swift
@@ -0,0 +1,43 @@
+//
+//  Locations.swift
+//  OnionApps
+//
+//  Created by Lyle Christianne Jover on 15/07/2016.
+//  Copyright © 2016 OnionApps. All rights reserved.
+//
+
+import UIKit
+
+class Locations: UIViewController {
+    
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Wiesenlust/LocationsCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationsCell.swift
+//  OnionApps
+//
+//  Created by Lyle Christianne Jover on 15/07/2016.
+//  Copyright © 2016 OnionApps. All rights reserved.
+//
+
+import UIKit
+
+class LocationsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
